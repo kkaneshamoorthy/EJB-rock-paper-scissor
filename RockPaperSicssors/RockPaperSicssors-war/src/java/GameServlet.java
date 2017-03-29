c15801ab7a9d25cd5f5b1556ed45d9b82280e214
Kowrishankar Kaneshamoorthy <K.Kowrishankar@yahoo.com>
2017-03-29 02:25:55 +0100

    Add files via upload

---
+++ RockPaperSicssors/RockPaperSicssors-war/src/java/GameServlet.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author kowrishankar
+ */
+@WebServlet(urlPatterns = {"/GameServlet"})
+public class GameServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        PrintWriter out = res.getWriter();
+        
+        out.append("Game starts in 3 ...");
+    }
+}
